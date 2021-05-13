ac8084ad7d9bf2bdfdae307719a4a74eb5ba3925
Sazia Makkar <saziamakkar19@gmail.com>
2021-05-13 12:17:33 -0700

    Allowing a user to post a comment and displaying it in the console

---
+++ private/html/JoinOrCreateTeam.js
@@ -0,0 +1,22 @@
+
+function getCode() {
+    console.log("working");
+    document.getElementById("Joinbutton").addEventListener('click', function () {
+        console.log("working");
+        var code = document.getElementById("code").value;
+        console.log("working");
+        console.log(code);
+
+				//read cities collection from firestore, with query
+        db.collection("groups")
+            .where("groupCode", "==", code)
+            .get()
+            .then(function (snap) {
+                snap.forEach(function(doc) {
+                    console.log(doc.data());
+                    //do something with the data
+                })
+            })
+    })
+}
+getCode();
