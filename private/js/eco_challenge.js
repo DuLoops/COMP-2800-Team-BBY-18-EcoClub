c6a793fafd7df04f91a096a2a0c6ebf4f79f179d
Fadi Basheer <fadih1992@gmail.com>
2021-05-18 09:21:05 -0700

    05-18

--- private/js/eco_challenge.js
+++ private/js/eco_challenge.js
@@ -1,22 +1,46 @@
 function createGrid() {
-    db.collection("eco-challenges").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id);
-            console.log(doc.data().desc);
-            var div = $("<div class='jumbotron'>" + doc.data().desc + "</div>");
-            var b1 = $("<a id='slot' type='button'></a><br>");
 
-            var head = $("<div class='form-group'><p class='chalange_name'>" + doc.id + "</p></div>");
-            b1.append(head);
-            b1.append(div);
-            b1.attr("href", "challenge_details.html").click(function () {
-                localStorage.setItem("id", doc.id);
-                localStorage.setItem("desc", doc.data().desc);
+    // firebase.auth().onAuthStateChanged(function (user) {
+    //     leader = (user.uid);
+    // });
+    // I should change the example with leader
+
+    db.collection("users").doc("example")
+        .collection("user-challenges").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id);
+                console.log(doc.data().desc);
+                // var div = $("<div class='jumbotron'>" + doc.data().desc + "</div>");
+                // var b1 = $("<a id='slot' type='button'></a><br>");
+
+                var div = $("<div class='form-group jumbotron'></div><br>");
+                var title = $("<p class='chalange_name'>" + doc.id + "></p>");
+                var complete = $("<button class='button button5'>Complete</button>");
+                var Delete = $("<button class='button button5'>Delete</button>");
+                complete.click(function () {
+                    window.location.href = "/private/html/group/group_feed.html";
+                    localStorage.setItem("id", doc.id);
+                    localStorage.setItem("desc", doc.data().desc);
+                });
+                Delete.click(async function () {
+                    await db.collection("users").doc("example")
+                        .collection("user-challenges").doc(doc.id).delete().then(() => {
+                            console.log("Document successfully deleted!");
+                        }).catch((error) => {
+                            console.error("Error removing document: ", error);
+                        });
+                    window.location.reload();
+                });
+
+
+
+
+                div.append(title);
+                div.append(complete);
+                div.append(Delete);
+                $("#list").append(div);
             });
-
-            $("#list").append(b1);
         });
-    });
 }
 createGrid();
