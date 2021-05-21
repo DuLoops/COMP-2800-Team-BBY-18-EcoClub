904c0918f97f734dc93de190be88a7afd7cd48b0
dujin kim <kdj00718@gmail.com>
2021-05-20 20:30:03 -0700

    sprint 3

--- private/js/challenge_details.js
+++ private/js/challenge_details.js
@@ -1,11 +1,16 @@
-function createGrid() {
-    var timeslocal = localStorage.getItem("desc");
-    // var times = JSON.parse(timeslocal);
-    // console.log(times[1][0]);
-    console.log(timeslocal);
-    document.getElementById("chalange_name_feild").innerHTML = timeslocal;
+function displayDetails() {
+    var docID = localStorage.getItem("docID");
+
+    db.collection("eco-challenges").doc(docID).get().then((doc)=>{
+        var title = doc.data().title;
+        var desc = doc.data().desc;
+        var ecopoint = doc.data().ecopoint;
+        document.getElementById("chalange_name_feild").innerHTML = title;
+        document.getElementById("comment").innerHTML = desc;
+        document.getElementById("eco_point_p").innerHTML = ecopoint + " eco-point";
+    });
 }
-createGrid();
+displayDetails();
 
 function take_challenge() {
     var challengeID = localStorage.getItem("id");
