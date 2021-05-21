a1deca714277b18b2c3a15a7d95c86812e40de33
dujin kim <kdj00718@gmail.com>
2021-05-21 11:41:34 -0700

    fd

--- private/js/eco_challenge.js
+++ private/js/eco_challenge.js
@@ -5,45 +5,6 @@ function createGrid() {
         db.collection("users").doc(user.uid)
             .collection("user_challenges").get().then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    // console.log(doc.id);
-                    // console.log(doc.data().isCompleted);
-                    // var div = $("<div class='jumbotron'>" + doc.data().desc + "</div>");
-                    // var b1 = $("<a id='slot' type='button'></a><br>");
-
-
-
-//                     var div = $("<div class='form-group'></div><br>");
-//                     var title = $("<p class='chalange_name'>" + doc.data().Title + "></p>");
-//                     var complete = $("<button class='button button5'>Complete</button>");
-//                     var Delete = $("<button class='button button5'>Delete</button>");
-
-//                     complete.click(async function () {
-//                         console.log(doc.id);
-//                         await db.collection("users").doc(user.uid).collection("user_challenges").doc(doc.id).update({
-//                             isCompleted: true
-//                         });
-
-//                         localStorage.setItem("challenge_title", doc.id);
-//                         window.location.href = "/private/html/challenges/eco_challenge_add_post.html";
-
-//                     });
-//                     Delete.click(async function () {
-//                         await db.collection("users").doc(user.uid)
-//                             .collection("user_challenges").doc(doc.id).delete().then(() => {
-//                                 console.log("Document successfully deleted!");
-//                             }).catch((error) => {
-//                                 console.error("Error removing document: ", error);
-//                             });
-//                         window.location.reload();
-
-//                     });
-//                     div.append(title);
-//                     div.append(complete);
-//                     div.append(Delete);
-//                     $("#list").append(div);
-//                 });
-
                     var id = doc.data().challengeID;
                     db.collection("eco-challenges").doc(id).get().then(function (doc) {
                         var name = doc.data().title;
@@ -51,30 +12,6 @@ function createGrid() {
                         var title = $("<p class='chalange_name'>" + name + "</p>");
                         var complete = $("<button class='button button5' onclick='CompleteChallenge(this)' challengeID='" + doc.id + "'>Complete</button>");
                         var Delete = $("<button class='button button5' onclick='DeleteChallenge(this)' challengeID='" + doc.id + "'>Delete</button>");
-
-                        // complete.click(async function () {
-                        //     console.log(doc.id);
-                        //     await db.collection("users").doc(user.uid).collection("user-challenges").doc(doc.id).update({
-                        //         isCompleted: true
-                        //     });
-
-                        //     localStorage.setItem("challenge_title", doc.id);
-                        //     window.location.href = "/private/html/challenges/eco_challenge_add_post.html";
-
-                        // });
-                        // Delete.click(async function () {
-
-                        //     // await db.collection("users").doc(user.uid)
-                        //     //     .collection("user_challenges").doc(id).delete().then(() => {
-                        //     //         console.log(id);
-                        //     //         console.log(user.uid);
-                        //     //         console.log("Document successfully deleted!");
-                        //     //     }).catch((error) => {
-                        //     //         console.error("Error removing document: ", error);
-                        //     //     });
-
-
-                        // });
                         div.append(title);
                         div.append(complete);
                         div.append(Delete);
