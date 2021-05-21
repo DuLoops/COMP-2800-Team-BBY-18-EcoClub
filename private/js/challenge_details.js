0dda977084ab4b99a9a104f87fcc2754467d7d92
Fadi Basheer <fadih1992@gmail.com>
2021-05-21 11:25:08 -0700

    05-21

--- private/js/challenge_details.js
+++ private/js/challenge_details.js
@@ -1,8 +1,9 @@
 function displayDetails() {
     var docID = localStorage.getItem("docID");
 
-    db.collection("eco-challenges").doc(docID).get().then((doc)=>{
+    db.collection("eco-challenges").doc(docID).get().then((doc) => {
         var title = doc.data().title;
+        console.log(title);
         var desc = doc.data().desc;
         var ecopoint = doc.data().ecopoint;
         document.getElementById("chalange_name_feild").innerHTML = title;
@@ -12,12 +13,34 @@ function displayDetails() {
 }
 displayDetails();
 
-function take_challenge() {
-    var challengeID = localStorage.getItem("id");
-    await firebase.auth().onAuthStateChanged(function (user) {
-        db.collection("users").doc(user.uid).collection("user_challenges").doc().set({
-            "challengeID": challengeID,
+async function take_challenge() {
+    var docID = localStorage.getItem("docID");
+    var title;
+    var desc;
+    var ecopoint;
+    var leader;
+    await firebase.auth().onAuthStateChanged(
+        function (user) {
+            console.log(user.uid);
+            leader = user.uid;
+
         });
+    await db.collection("eco-challenges").doc(docID).get().then((doc) => {
+        title = doc.data().title;
+        desc = doc.data().desc;
+        ecopoint = doc.data().ecopoint;
     });
- window.location.href = "/private/html/challenges/eco_challenge.html";
+    await db.collection("users").doc(leader).collection("user_challenges").doc().set({
+        Title: title,
+        Desc: desc,
+        Ecopoints: ecopoint
+    });
+
+
+
+
+
+
+
+    window.location.href = "/private/html/challenges/eco_challenge.html";
 }
--- private/js/eco_challenge.js
+++ private/js/eco_challenge.js
@@ -2,45 +2,46 @@ function createGrid() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         db.collection("users").doc(user.uid)
-        .collection("user_challenges").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                // console.log(doc.id);
-                // console.log(doc.data().isCompleted);
-                // var div = $("<div class='jumbotron'>" + doc.data().desc + "</div>");
-                // var b1 = $("<a id='slot' type='button'></a><br>");
+            .collection("user_challenges").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    // console.log(doc.id);
+                    // console.log(doc.data().isCompleted);
+                    // var div = $("<div class='jumbotron'>" + doc.data().desc + "</div>");
+                    // var b1 = $("<a id='slot' type='button'></a><br>");
 
-                var div = $("<div class='form-group'></div><br>");
-                var title = $("<p class='chalange_name'>" + doc.id + "></p>");
-                var complete = $("<button class='button button5'>Complete</button>");
-                var Delete = $("<button class='button button5'>Delete</button>");
 
-                complete.click(async function () {
-                    console.log(doc.id);
-                    await db.collection("users").doc(user.uid).collection("user-challenges").doc(doc.id).update({
-                        isCompleted: true
-                    });
+                    var div = $("<div class='form-group'></div><br>");
+                    var title = $("<p class='chalange_name'>" + doc.data().Title + "></p>");
+                    var complete = $("<button class='button button5'>Complete</button>");
+                    var Delete = $("<button class='button button5'>Delete</button>");
 
-                    localStorage.setItem("challenge_title", doc.id);
-                    window.location.href = "/private/html/challenges/eco_challenge_add_post.html";
-
-                });
-                Delete.click(async function () {
-                    await db.collection("users").doc(user.uid)
-                        .collection("user-challenges").doc(doc.id).delete().then(() => {
-                            console.log("Document successfully deleted!");
-                        }).catch((error) => {
-                            console.error("Error removing document: ", error);
+                    complete.click(async function () {
+                        console.log(doc.id);
+                        await db.collection("users").doc(user.uid).collection("user_challenges").doc(doc.id).update({
+                            isCompleted: true
                         });
-                    window.location.reload();
 
+                        localStorage.setItem("challenge_title", doc.id);
+                        window.location.href = "/private/html/challenges/eco_challenge_add_post.html";
+
+                    });
+                    Delete.click(async function () {
+                        await db.collection("users").doc(user.uid)
+                            .collection("user_challenges").doc(doc.id).delete().then(() => {
+                                console.log("Document successfully deleted!");
+                            }).catch((error) => {
+                                console.error("Error removing document: ", error);
+                            });
+                        window.location.reload();
+
+                    });
+                    div.append(title);
+                    div.append(complete);
+                    div.append(Delete);
+                    $("#list").append(div);
                 });
-                div.append(title);
-                div.append(complete);
-                div.append(Delete);
-                $("#list").append(div);
             });
-        });
 
     });
     // I should change the example with leader
