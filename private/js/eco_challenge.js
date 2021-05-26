2d86e3668e43cd585264de9ef260e6d3a4069be9
Sazia Makkar <saziamakkar19@gmail.com>
2021-05-26 10:48:14 -0700

    Displayed description of eco challenges and changed the display

--- private/js/eco_challenge.js
+++ private/js/eco_challenge.js
@@ -7,17 +7,24 @@ function createGrid() {
                     var id = doc.data().challengeID;
                     db.collection("eco-challenges").doc(id).get().then(function (doc) {
                         var name = doc.data().title;
-                        var div = $("<div class='form-group'></div><br>");
-                        var title = $("<p class='chalange_name'>" + name + "</p>");
-                        var complete = $("<button class='button button5' onclick='CompleteChallenge(this)' challengeID='" + doc.id + "'>Complete</button>");
-                        var Delete = $("<button class='button button5' onclick='DeleteChallenge(this)' challengeID='" + doc.id + "'>Delete</button>");
-                        div.append(title);
-                        div.append(complete);
-                        div.append(Delete);
-                        $("#list").append(div);
+                        var desc = doc.data().desc;
+                        // var div = $("<div class='form-group'></div><br>");
+                        // var title = $("<p class='chalange_name'>" + name + "</p>");
+                        // var complete = $("<button class='button button5' onclick='CompleteChallenge(this)' challengeID='" + doc.id + "'>Complete</button>");
+                        // var Delete = $("<button class='button button5' onclick='DeleteChallenge(this)' challengeID='" + doc.id + "'>Delete</button>");
+                        // div.append(title);
+                        // div.append(complete);
+                        // div.append(Delete);
+                        // $("#list").append(div);
+                        document.getElementById("list").innerHTML += "<div class='accordion accordion-flush' id='accordionFlushExample'><div class='accordion-item'>" 
+                        + " <h2 class='accordion-header' id='flush-headingOne'> <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#flush-collapseOne' aria-expanded='false' aria-controls='flush-collapseOne'>" 
+                        + name + "</button></h2><div id='flush-collapseOne' class='accordion-collapse collapse' aria-labelledby='flush-headingOne' data-bs-parent='#accordionFlushExample'>" 
+                        + "<div class=' accordion-body'>" + desc + "<br><br><button class='btn button button5' onclick='CompleteChallenge(this)' challengeID='" + doc.id + "'>Complete</button><button class='btn button button5' onclick='DeleteChallenge(this)' challengeID='" + doc.id + "'>Delete</button></div></div></div>"
                     });
                 })
+
             });
+            document.getElementById("list").innerHTML += "</div>";
     });
 }
 
@@ -43,17 +50,16 @@ function Completed() {
                     var id = doc.data().challengeID;
                     db.collection("eco-challenges").doc(id).get().then(function (doc) {
                         var name = doc.data().title;
-                        var div = $("<div class='form-group'></div><br>");
-                        var title = $("<p class='chalange_name'>" + name + "</p>");
-                        var complete = $("<button class='button button5' onclick='CompleteChallenge(this)' challengeID='" + doc.id + "'>Complete</button>");
-                        var Delete = $("<button class='button button5' onclick='DeleteChallenge(this)' challengeID='" + doc.id + "'>Delete</button>");
-                        div.append(title);
-                        div.append(complete);
-                        div.append(Delete);
-                        $("#list").append(div);
+                        var desc = doc.data().desc;
+                        
+                        document.getElementById("list").innerHTML += "<div class='accordion accordion-flush' id='accordionFlushExample'><div class='accordion-item' >" 
+                        + " <h2 class='accordion-header' id='flush-headingOne'> <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#flush-collapseOne' aria-expanded='false' aria-controls='flush-collapseOne'>" 
+                        + name + "</button></h2><div id='flush-collapseOne' class='accordion-collapse collapse' aria-labelledby='flush-headingOne' data-bs-parent='#accordionFlushExample'>" 
+                        + "<div class=' accordion-body'>" + desc + "</div></div></div>"
                     });
                 })
             });
+            document.getElementById("list").innerHTML += "</div>";
     });
 }
 
